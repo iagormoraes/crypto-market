752e8abdf19fdeee65eb7b7c50bc7edc14336819
iagor moraes <iagor2014@outlook.com>
2022-04-25 15:28:34 -0300

    chore: add environment variables

---
+++ server/src/config/environment.ts
@@ -0,0 +1,5 @@
+export const Environment = {
+  DATABASE_NAME: process.env.DATABASE_NAME ?? '',
+  ACCESS_TOKEN_EXPIRY: process.env.ACCESS_TOKEN_EXPIRY ?? '',
+  JWT_SECRET: process.env.JWT_SECRET ?? '',
+};
