a06057713b6e640c5cd854f33ef705c3b06e9fad
iagor moraes <iagor2014@outlook.com>
2022-04-25 19:25:50 -0300

    refactor: validate user with bcrypt

--- server/src/models/auth/auth.service.ts
+++ server/src/models/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 import { UsersService } from '../users/users.service';
 import { User } from '../users/interfaces/user.inteface';
@@ -11,10 +12,17 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async validateUser(username, password: string): Promise<any> {
+  async validateUser(
+    username,
+    password: string,
+  ): Promise<Omit<User, 'password'> | null> {
     const user = await this.usersService.findOne(username);
 
-    if (user && user.password == password) {
+    if (!user) return null;
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (isMatch) {
       const { password, ...result } = user;
 
       return result;
@@ -25,9 +33,12 @@ export class AuthService {
 
   async login(user: User) {
     const payload = { id: user.id, email: user.email, role: user.role };
+    const token = this.jwtService.sign(payload);
+    const { exp } = this.jwtService.verify(token);
 
     return {
-      access_token: this.jwtService.sign(payload),
+      access_token: token,
+      access_token_expiration: new Date(exp * 1000).toISOString(),
     };
   }
 }
