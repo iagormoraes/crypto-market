9d91e94b55d98eb91dbea53e5ea4230e7ecd51b9
iagor moraes <iagor2014@outlook.com>
2022-04-25 22:00:33 -0300

    refactor: remove admin role guard

--- server/src/models/users/users.controller.ts
+++ server/src/models/users/users.controller.ts
@@ -1,13 +1,11 @@
 import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 
-import { JwtAuthGuard, RolesGuard } from '../auth/guards';
-import { Role } from '../roles/role.enum';
+import { JwtAuthGuard } from '../auth/guards';
 
 @UseGuards(JwtAuthGuard)
 @Controller('users')
 export class UsersController {
   @Get('user/profile')
-  @UseGuards(RolesGuard(Role.Admin))
   getProfile(@Request() req) {
     return req.user;
   }
