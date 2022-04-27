3e64c60a0a76cbb911eb1f87b1dc4e1489648ee3
iagor moraes <iagor2014@outlook.com>
2022-04-27 11:41:44 -0300

    chore: setup tailwind

---
+++ client/tailwind.config.js
@@ -0,0 +1,20 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  mode: 'jit',
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './src/commons/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    colors: {
+      ...colors,
+    },
+    fontFamily: {
+      primary: ['Inter', 'Arial', 'sans-serif'],
+      secondary: ['Lora', 'Times New Roman', 'Times', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: ['tailwindcss', 'autoprefixer', 'postcss'],
+};
