c1b6e8039575e9e86e88a9b78a73feb1b2e2419e
iagor moraes <iagor2014@outlook.com>
2022-04-28 14:13:59 -0300

    refactor: render auth buttons conditionally

--- client/src/commons/containers/NavigationBar/index.tsx
+++ client/src/commons/containers/NavigationBar/index.tsx
@@ -1,10 +1,15 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { signOut } from 'next-auth/react';
 import styles from './style.module.css';
 
 import Button, { ButtonVariant } from '../../components/buttons/Button';
 
+import useAppSession, { AppSessionState } from '../../../hooks/useAppSession';
+
 export default function NavigationBar() {
+  const { status, session } = useAppSession();
+
   return (
     <nav className={styles.nav}>
       <div className="container m-auto flex items-center px-2">
@@ -19,18 +24,34 @@ export default function NavigationBar() {
         <h1 className="ml-2 text-white text-lg font-bold">Crypto Market</h1>
         <nav className="ml-auto">
           <ul className="flex items-center gap-4">
-            <li>
-              <Link href="/login" passHref>
-                <Button variant={ButtonVariant.Small}>Login</Button>
-              </Link>
-            </li>
-            <li>
-              <Link href="/register" passHref>
-                <Button variant={ButtonVariant.Small} alt>
-                  Register
+            {status === AppSessionState.Unauthenticated && (
+              <li>
+                <Link href="/login" passHref>
+                  <Button variant={ButtonVariant.Small}>Login</Button>
+                </Link>
+              </li>
+            )}
+            {status === AppSessionState.Unauthenticated && (
+              <li>
+                <Link href="/register" passHref>
+                  <Button variant={ButtonVariant.Small} alt>
+                    Register
+                  </Button>
+                </Link>
+              </li>
+            )}
+
+            {status === AppSessionState.Authenticated && (
+              <li>
+                <Button
+                  variant={ButtonVariant.Small}
+                  alt
+                  onClick={() => signOut({ redirect: false })}
+                >
+                  Logout
                 </Button>
-              </Link>
-            </li>
+              </li>
+            )}
           </ul>
         </nav>
       </div>
