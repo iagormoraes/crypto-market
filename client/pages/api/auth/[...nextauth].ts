9e71794eac6dec07960f7ca9e1c5a2bd32e42ae1
iagor moraes <iagor2014@outlook.com>
2022-04-28 14:01:04 -0300

    refactor: remove unused param

--- client/pages/api/auth/[...nextauth].ts
+++ client/pages/api/auth/[...nextauth].ts
@@ -25,7 +25,7 @@ export default NextAuth({
         },
         password: { label: 'Password', type: 'password' },
       },
-      async authorize(credentials, req) {
+      async authorize(credentials) {
         try {
           const { data: response } = await httpClientExternal.request({
             url: '/auth/login',
