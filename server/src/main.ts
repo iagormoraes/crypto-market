e28e70df95de319797cb1bc6449d0b33c8696737
iagor moraes <iagor2014@outlook.com>
2022-04-27 23:19:53 -0300

    refactor: enable cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,7 @@ import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useWebSocketAdapter(new WsAdapter(app));
   await app.listen(3000);
