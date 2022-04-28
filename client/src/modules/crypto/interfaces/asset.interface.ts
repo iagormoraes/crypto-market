1202a30f81cce6f99d07b7e0de05216e03afc7c8
iagor moraes <iagor2014@outlook.com>
2022-04-28 18:28:38 -0300

    chore: create crypto asset

---
+++ client/src/modules/crypto/interfaces/asset.interface.ts
@@ -0,0 +1,12 @@
+export interface CryptoAsset {
+  ask: string;
+  assetCode: string;
+  bid: string;
+  close: string;
+  high: string;
+  low: string;
+  lowLast24: string;
+  open: string;
+  openLast24: string;
+  volume: string;
+}
