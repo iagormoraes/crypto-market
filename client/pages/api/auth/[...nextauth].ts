5b0e60988c4b84b66bba20bd1004c2a3a2d3daab
iagor moraes <iagor2014@outlook.com>
2022-04-28 12:47:00 -0300

    chore: setup auth provider

--- client/pages/api/auth/[...nextauth].ts
+++ client/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,19 @@
-import NextAuth from 'next-auth';
+import NextAuth, { User } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 
 import { httpClientExternal } from '../../../src/lib/http-client/http-client-external';
+import { LoginUserDto } from '../../../src/modules/user/dtos/login-user.dto';
+import { UserMapper } from '../../../src/modules/user/mappers/user.mapper';
 
 export default NextAuth({
+  secret: 'some secret',
+  pages: {
+    signIn: '/login',
+    error: '/login',
+  },
+  session: {
+    strategy: 'jwt',
+  },
   providers: [
     CredentialsProvider({
       name: 'Credentials',
@@ -16,21 +26,41 @@ export default NextAuth({
         password: { label: 'Password', type: 'password' },
       },
       async authorize(credentials, req) {
-        let res = null;
-
         try {
           const { data: response } = await httpClientExternal.request({
             url: '/auth/login',
             method: 'POST',
+            data: credentials,
           });
 
-          res = response.data;
-        } catch (error) {
-          res = null;
+          return response.data;
+        } catch (error: any) {
+          throw new Error(error.response.data.message);
         }
-
-        return res;
       },
     }),
   ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        return {
+          user: new UserMapper().toDomain(user.profile as LoginUserDto),
+          accessToken: (user.token as Record<string, any>)
+            .access_token as string,
+          accessTokenExpires: +new Date(
+            (user.token as Record<string, any>).access_token_expiration,
+          ),
+        };
+      }
+
+      return token;
+    },
+    async session({ session, token }) {
+      session.user = token.user as User;
+      session.accessToken = token.accessToken;
+      session.error = token.error;
+
+      return session;
+    },
+  },
 });
