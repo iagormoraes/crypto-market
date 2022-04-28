79e702a1a960ce2fe0e672408d66fe4535e767d6
iagor moraes <iagor2014@outlook.com>
2022-04-28 18:28:23 -0300

    fix: return page props properly

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import '../src/commons/styles/globals.css';
 
-function MyApp({ Component, pageProps: { session, pageProps } }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
