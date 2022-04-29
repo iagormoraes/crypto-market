ce2ab66ce2176a966d5572ed87250f6cb3dc9769
iagor moraes <iagor2014@outlook.com>
2022-04-28 23:30:19 -0300

    test: add test for NavigationBar

--- client/src/commons/containers/NavigationBar/NavigationBar.test.tsx
+++ client/src/commons/containers/NavigationBar/NavigationBar.test.tsx
@@ -1,10 +1,31 @@
 import { render } from '@testing-library/react';
-
 import NavigationBar from '.';
+import { SessionProvider } from 'next-auth/react';
+
+global.fetch = jest.fn(
+  (): Promise<any> =>
+    Promise.resolve({
+      json: () => Promise.resolve({}),
+    }),
+);
+
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      route: '/',
+      pathname: '',
+      query: '',
+      asPath: '',
+      push: jest.fn(),
+    };
+  },
+}));
 
 describe('<NavigationBar/>', () => {
-  it('matches snapshot', () => {
-    const { container } = render(<NavigationBar />);
+  it('matches snapshot on loading', () => {
+    const { container } = render(<NavigationBar />, {
+      wrapper: ({ children }) => <SessionProvider>{children}</SessionProvider>,
+    });
 
     expect(container).toMatchSnapshot();
   });
